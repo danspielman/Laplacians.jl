c3fef2308591e8a39a0bad0307ffadbd795ec445
Dan Spielman <spielman@cs.yale.edu>
2016-01-03 22:39:14 -0500

    First draft of KMP Solver that almost works
    Just for the Laplacian case.
    Might be a bug in the construction of ind, etc.

--- devel/kmpTest.jl
+++ devel/kmpTest.jl
@@ -1,30 +1,3 @@
-immutable elimLeafNode
-    nodeid::Int64
-    parent::Int64
-    wtDeg::Float64
-end
-
-immutable elimDeg2Node
-    nodeid::Int64
-    nbr1::Int64
-    nbr2::Int64
-    wt1::Float64
-    wt2::Float64
-end
-
-
-function makeElimList(t)
-    tr = matToTree(t)
-    n = size(tr.children,1)  
-    
-    elims = Array{elimLeafNode}(0)
-    for vi in n:-1:2
-        v = tr.children[vi]
-        push!(elims,elimLeafNode(v,tr.parent[v],tr.weights[vi]))
-    end
-    
-    return elims
-end
 
 function solveTree(elims,b)
     cumb = copy(b)
@@ -41,120 +14,6 @@ function solveTree(elims,b)
     
 end
 
-# Must be in DFS order
-# marked is 1 if flagged for possible elimination,
-# and set to 2 if we do eliminate it
-function elimDeg1(t, marked)
-    n = t.n
-
-    deg = Array{Int64}(n)
-    for v in 1:n
-        deg[v] = t.colptr[v+1] - t.colptr[v]
-    end
-
-    elims1 = Array{elimLeafNode}(0)
-
-    for v in n:-1:2
-
-        if (deg[v] == 1 && marked[v] == 1)
-            parent = t.rowval[t.colptr[v]];
-            wt = t.nzval[t.colptr[v]];
-            push!(elims1,elimLeafNode(v,parent,wt))
-
-            deg[parent] = deg[parent] - 1
-            marked[v] = 2
-        end
-    end
-    return elims1
-end
-
-
-# Must be in DFS order
-# marked is 1 if flagged for possible elimination,
-# and set to 2 if we do eliminate it
-function elimDeg12(t, marked)
-
-    # make sure root is not marked
-    marked[1] = 0
-
-    deg = Array{Int64}(n)
-    for v in 1:n
-        deg[v] = t.colptr[v+1] - t.colptr[v]
-    end
-
-    elims1 = Array{elimLeafNode}(0)
-
-    for v in n:-1:2
-
-        if (deg[v] == 1 && marked[v] == 1)
-            parent = t.rowval[t.colptr[v]];
-            wt = t.nzval[t.colptr[v]];
-            push!(elims1,elimLeafNode(v,parent,wt))
-
-            deg[parent] = deg[parent] - 1
-            marked[v] = 2
-            deg[v] = 0
-        end
-    end
-
-    elims2 = Array{elimDeg2Node}(0)
-
-    subt = triu(t)
-
-    for v in n:-1:2
-
-        if (deg[v] == 2 && marked[v] == 1)
-
-            parent = t.rowval[t.colptr[v]];
-
-            # to ident the child, enumerate to find one uneliminated, which we check by marked
-            kidind = t.colptr[v]+1
-            kid = t.rowval[kidind]
-            while deg[kid] == 0
-                kidind = kidind+1
-                kid = t.rowval[kidind]
-
-                if kidind >= t.colptr[v+1]
-                    error("went of the end of the kid list without finding node to elim from")
-                end
-            end
-
-
-            wt1 = t.nzval[t.colptr[v]];
-            wt2 = t.nzval[kidind]
-
-            push!(elims2,elimDeg2Node(v,parent,kid,wt1,wt2))
-            marked[v] = 2
-
-            newwt = 1/(1/wt1 + 1/wt2)
-
-            # now that we've found the kid, go up the chain until done
-            while (deg[parent] == 2 && marked[parent] == 1)
-                v = parent
-                parent = t.rowval[t.colptr[v]];
-                wt1 = t.nzval[t.colptr[v]];
-                wt2 = newwt
-
-                push!(elims2,elimDeg2Node(v,parent,kid,wt1,wt2))
-                marked[v] = 2
-                
-                newwt = 1/(1/wt1 + 1/wt2)
-            end
-
-            # now, hack the tree to adjust parent and wt of node kid
-            subt.rowval[subt.colptr[kid]] = parent
-            subt.nzval[subt.colptr[kid]] = newwt
-
-        end
-    end
-
-    subt = subt + subt'
-
-    ind = find(marked.<2)
-    subt = subt[ind,ind]
-    
-    return elims1, elims2, ind, subt
-end
 
 
 function recTest1(t,b, marked)
@@ -248,43 +107,6 @@ function recTest12(t,b, marked)
 end
 
 
-
-function forwardSolve(b, elims1, elims2)
-
-    y = copy(b)
-
-    for i in 1:length(elims1)
-        y[elims1[i].parent] += y[elims1[i].nodeid]
-    end
-
-    for i in 1:length(elims2)
-        wtsum = elims2[i].wt1 + elims2[i].wt2
-        y[elims2[i].nbr1] += y[elims2[i].nodeid]*elims2[i].wt1 / wtsum
-        y[elims2[i].nbr2] += y[elims2[i].nodeid]*elims2[i].wt2 / wtsum
-    end
-
-    return y
-    
-end
-
-function backSolve(x, y, elims1, elims2)
-    
-    for i in length(elims2):-1:1
-        node = elims2[i].nodeid
-        wtsum = elims2[i].wt1 + elims2[i].wt2
-
-        x[node] = (elims2[i].wt1*x[elims2[i].nbr1] + elims2[i].wt2*x[elims2[i].nbr2] + y[node])/wtsum
-    end
-
-    
-    for i in length(elims1):-1:1
-        node = elims1[i].nodeid
-        x[node] = x[elims1[i].parent] + y[node]/elims1[i].wtDeg
-    end
-
-end
-
-
 function recTest12x(t,b, marked)
 
     n = length(b)
@@ -314,6 +136,45 @@ function recTest12x(t,b, marked)
 end
 
 
+
+function testSampler(a; t=akpw(a), frac1=1/5)
+
+
+    n = size(a,1);
+
+    rest = a-t;
+
+    st = compStretches(t,rest);
+    aveStretch = sum(st)/nnz(rest)
+    @show aveStretch
+    
+
+    targetStretch = 1/(2*log(n)/log(2))
+
+    fac = aveStretch/targetStretch
+    heavy = rest+fac*t;
+
+    (ai,aj,av) = findnz(triu(rest))
+    (si,sj,sv) = findnz(triu(st))
+    sv = sv ./ fac
+
+    ijvs = IJVS(ai,aj,av,sv)
+
+    ijvs1 = stretchSample(ijvs,targetStretch,frac1)
+    ijvs2 = stretchSample(ijvs1,targetStretch,frac1)
+    
+    samp1 = sparse(ijvs1.i,ijvs1.j,ijvs1.v,n,n)
+    samp1 = samp1 + samp1';
+    
+    samp2 = sparse(ijvs2.i,ijvs2.j,ijvs2.v,n,n)
+    samp2 = samp2 + samp2';
+    
+    add = speye(n)/10^6;
+    e1 = maximum(eigs(lap(heavy)+add,lap(samp1+t*fac)+add, nev=1)[1]);
+    e2 = maximum(eigs(lap(samp1+t*fac)+add,lap(samp2+t*fac)+add, nev=1)[1]);
+
+    return [e1,e2]
+end
 
 
 
@@ -379,4 +240,8 @@ function testAtSize(n, results; frac1=1/3, frac2 = 1/15)
     end
 
     
+end
+
+function vecstats(s)
+    println("length : ", size(s,1), ", min : ", minimum(s), ", mean : ", mean(s), ", max : ", maximum(s), ", sum : ", sum(s))
 end
