24313d0ed55a6e5c9a49e7da86573aca9ccf6157
Rasmus Kyng <rasmus.kyng@yale.edu>
2016-04-16 14:37:16 -0400

    working on sqLinOpWrapper

--- src/sqLinOpWrapper.jl
+++ src/sqLinOpWrapper.jl
@@ -1,6 +1,7 @@
 import Base.*
 import Base.size
 import Base.eltype
+import Base.issym
 
 immutable SqLinOp{Tv,Ti}
     issym::Bool
@@ -17,10 +18,14 @@ eltype{Tv,Ti}(A::SqLinOp{Tv,Ti}) = Tv
 
 size{Tv,Ti}(A::SqLinOp{Tv,Ti}, d::Ti) = A.n
 
+size{Tv,Ti}(A::SqLinOp{Tv,Ti}) = (A.n,A.n)
+
+issym{Tv,Ti}(A::SqLinOp{Tv,Ti}) = A.issym
+
 function *{Tv,Ti}(A::SqLinOp{Tv,Ti}, b::Array{Tv,1})
     return A.multFn(b)
 end
 
-function testId()
-    return M = SqLinOp{Float64,Int64}(true,1.0,1,x -> x)
+function testId{Ti}(n::Ti)
+    return M = SqLinOp{Float64,Int64}(true,1.0,n,x -> x)
 end
