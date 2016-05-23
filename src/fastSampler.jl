18b685c9f8f54d6d1f5c8148e989d419ff2c83b6
Rasmus Kyng <rasmus.kyng@yale.edu>
2016-05-23 18:23:45 -0400

    working on fastSampler err

--- src/fastSampler.jl
+++ src/fastSampler.jl
@@ -1,105 +1,139 @@
-#TODO: would be nice to add a link to the paper rasmus was referencing	
-immutable Sampler{Tv,Ti}
-	F::Array{Tv,1}	# array of p[s.A[i]] / (1 / n)
-	A::Array{Ti,1}	# array of indices of elements < (1 / n)
-	V::Array{Ti,1}	# arary of indices of elements > (1 / n)
-	n
+#TODO: would be nice to add a link to the paper rasmus was referencing    
+immutable NewSampler{Tv,Ti}
+    F::Array{Tv,1}    # array of p[s.A[i]] / (1 / n)
+    A::Array{Ti,1}    # array of indices of elements < (1 / n)
+    V::Array{Ti,1}    # arary of indices of elements > (1 / n)
+    n::Ti
 end
 
 # immutable Sampler
-# 	F::Array{Float64,1}	# array of p[s.A[i]] / (1 / n)
-# 	A::Array{Int64,1}	# array of indices of elements < (1 / n)
-# 	V::Array{Int64,1}	# arary of indices of elements > (1 / n)
-# 	n
+#     F::Array{Float64,1}    # array of p[s.A[i]] / (1 / n)
+#     A::Array{Int64,1}    # array of indices of elements < (1 / n)
+#     V::Array{Int64,1}    # arary of indices of elements > (1 / n)
+#     n
 # end
 
 # sample a random number
-function sample(s::Sampler)
+function newSample{Tv,Ti}(s::NewSampler{Tv,Ti})
+    #i = rand(1:s.n)
+    i = ceil(Ti,rand()*s.n)
+    f = rand()
+    if f < s.F[i]
+        return s.A[i]
+    else
+        return s.V[i]
+    end
 
-	i = rand(1:s.n)
-	f = rand()
-	if f < s.F[i]
-		return s.A[i]
-	else
-		return s.V[i]
-	end
+end
 
+# sample a random number
+# 
+function newSampleMany{Tv,Ti}(s::NewSampler{Tv,Ti},sampCount::Ti)
+    #i = rand(1:s.n)
+    samples = Array{Ti,1}(sampCount)
+    for j = 1:sampCount
+        i = ceil(Ti,rand()*s.n)
+        f = rand()
+        if f < s.F[i]
+            samples[j] = s.A[i]
+        else
+            samples[j] = s.V[i]
+        end
+    end
+    return samples
 end
 
 # initialize the sampler. To get the residual error after building the sampler, set residual to true
-function sampler{Tv}(p::Array{Tv,1}; residual::Bool = false)
+function newSampler{Tv}(p::Array{Tv,1}; residual::Bool = false)
 
-	n = length(p)
+    n = length(p)
 
-	# generate the Sampler
-	F = Array{Tv}(n)
-	A = Array{Int32}(n)
-	V = Array{Int32}(n)
-	
-	# generate a normalized p, so that it sums up to n
-	newp = n * copy(p) / sum(p)
+    # generate the Sampler
+    F = Array{Tv}(n)
+    A = Array{Int64}(n)
+    V = Array{Int64}(n)
+    
+    # generate a normalized p, so that it sums up to n
+    newp = n * copy(p) / sum(p)
 
-	# fill up the arays A and V
-	pos = 0						# number of filled or half filled buckets
-	posFilled = 0				# number of filled buckets
+    # fill up the arays A and V
+    pos = 0                        # number of filled or half filled buckets
+    posFilled = 0                # number of filled buckets
 
-	for i in 1:n
-		if newp[i] < 1 && !epsequal(newp[i], 1)
-			pos = pos + 1
-			A[pos] = i
-			F[pos] = newp[i]
-		end
-	end
+    for i in 1:n
+        if newp[i] < 1 #&& !epsequal(newp[i], 1)
+            pos = pos + 1
+            A[pos] = i
+            F[pos] = newp[i]
+        end
+    end
 
-	residualError = 0
+    #residualError = 0
 
-	for i in 1:n
-		if newp[i] >= 1 || epsequal(newp[i], 1)
-			val = newp[i]
+    # printErr = 0
+    
+    for i in 1:n
+        if newp[i] >= 1 #|| epsequal(newp[i], 1)
+            val = newp[i]
 
-			while val >= 1 || epsequal(val, 1)
-				if posFilled < pos
-					# fill up a bucket already containing a small element, and subtract that value from the current element
-					posFilled = posFilled + 1
-					V[posFilled] = i
-					val = val - (1 - F[posFilled])
-				else
-					# create a bucket with only one large element. note posFilled = pos
-					pos = pos + 1
-					posFilled = posFilled + 1
-					A[pos] = V[pos] = i
-					F[pos] = 1
-					val = val - 1
-				end
-			end
+            # if pos == n && i < n
+            #     printErr = 1
+            # end
+            
+            while val >= 1 #|| epsequal(val, 1)
+                if posFilled < pos
+                    # fill up a bucket already containing a small element, and subtract that value from the current element
+                    posFilled = posFilled + 1
+                    V[posFilled] = i
+                    val = val - (1 - F[posFilled])
+                else
+                    # if pos == n
+                    #     writedlm("boundsErr.txt",p)
+                    # end
+                    
+                    # create a bucket with only one large element. note posFilled = pos
+                    pos = pos + 1
+                    posFilled = posFilled + 1
+                    A[pos] = V[pos] = i
+                    F[pos] = 1
+                    val = val - 1
+                end
+            end
 
-			# treat the case in which val becomes smaller than 1. Have to do the pos < n check because of precision errors
-			if val > 0 && !epsequal(val, 0) && pos < n
-				pos = pos + 1
-				A[pos] = i
+            # treat the case in which val becomes smaller than 1. Have to do the pos < n check because of precision errors
+            if val > 0 && pos < n #&& !epsequal(val, 0) && pos < n
+                pos = pos + 1
+                A[pos] = i
+                F[pos] = val
+            end
 
-				if pos == n
-					# if we are in this case, posFilled should be equal to pos - 1
-					posFilled = posFilled + 1
-					V[posFilled] = i
-				end
+            # residualError += val
+        end
+    end
 
-				F[pos] = val
-			end
+    err = 0
+    while posFilled < n
+        # if we are in this case, posFilled should be equal to pos - 1
+        posFilled = posFilled + 1
+        V[posFilled] = A[posFilled]
+        err += 1-F[posFilled]
+    end
 
-			residualError += val
-		end
-	end
+    # if printErr != 0
+    #     println("residual error = ", err)
+    # end
 
-	@assert(posFilled == pos, "pos and posFilled differ")
+    #println("err = ",err)
+    
+    #@assert(posFilled == pos, "pos and posFilled differ")
 
-	if residual
-		return Sampler(F, A, V, n), residualError
-	else
-		return Sampler(F, A, V, n)
-	end
+    if residual
+        return NewSampler(F, A, V, n), err
+    else
+        return NewSampler(F, A, V, n)
+    end
 end
 
-function epsequal(a, b; eps=1e-15)
-	return abs(a - b) < eps
+function epsequal(a, b; eps=1e-17)
+    return abs(a - b) <= eps
 end
