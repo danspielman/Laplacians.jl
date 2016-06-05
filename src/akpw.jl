08cc330fcff0665f93dfe7ace680721b4cac7e32
Dan Spielman <spielman@cs.yale.edu>
2016-06-05 12:29:52 -0400

    simplified the code: got rid of fancy orders for seeds

--- src/akpw.jl
+++ src/akpw.jl
@@ -4,52 +4,6 @@ for the unwted case first:
 do a bfs from an initial vertex.
 label vertices 0 initially, and eventually by component number.
 
-as bfs, keep a boundary list.
-
-when finish making the cluster, go down the boundary list of find the next vertex.
-
-issue: some vertices could get added to be boundary list multiple times.
-  there are two ways to deal with this:
-
-1. start with all nodes labeled -1
-when a node reaches a boundary, label it 0.
-when in clusters, get positive numbers.
-so, we never put in boundary twice.
-
-we will need a reusable queue to keep track of current boundaries.
-
-So, will be two queues: oldboundaries and currentboundary
-
-go until bdry to vol ratio goes down enough
-to keep track of vol and bdry:
-  every time we add a new node to the component,
-  we traverse its edges.
-  each edge pointing in increases vol, and decreases bdry (was prev bdry)
-  each edge pointing out increases bdry
- sounds simple enough
-
-akpwU(graph, startNode)
-
-init
-  create boundary queue
-  fastQueue has
-    fixed initial size
-    the queue
-    endPtr
-    curPtr
-
-    cur is where are,
-    endPtr is the last item
-
-  init the comp vector to -1
-  0 for bdry
-  others for comp number
-
-then, need a grow op.
-  it gets to ref comp, both queues, and the graph
-
-  it should be called with a seed node.
-  So, before call, is a while loop to check on bdry.
 
 =#
 
@@ -157,14 +111,14 @@ function akpwU(graph, xfac::Function)
 
 end
 
-akpwU(graph) = akpwU(graph, 0.2)
+akpwU(graph) = akpwU(graph, x->(1/(2*log(x))))
 
 
 function akpwUsub(graph, xfac::Function)
     n = size(graph,1)
     m = nnz(graph)
     
-    comp = -ones(Int, n)
+    comp = zeros(Int, n)
     ncomps = 0
     
     # the indices into (ai,aj) = findnz(a) of the edges in the tree
@@ -173,24 +127,16 @@ function akpwUsub(graph, xfac::Function)
     curBdry = fastQueue(n)
     
     # start from a random vertex
-    potSeeds = fastQueue(n)
-    push!(potSeeds,rand(1:n))
+    seedlist = randperm(n)
     
     # allocate reusable queue, but only used inside subroutines
     thisBdry = fastPairQueue(m)
 
-    hackSeed = 1
-    
-    while hackSeed <= n # hasMore(potSeeds)
+    for seed in seedlist
 
-        # could try pulling a random seed instead
-        # seed = pull!(potSeeds)
-        seed = hackSeed
-        hackSeed += 1
-        
         if comp[seed] <= 0
             ncomps += 1
-            growFromSeed(graph, seed, ncomps, comp, treeEdges, thisBdry, potSeeds, xfac(n)) 
+            growFromSeed(graph, seed, ncomps, comp, treeEdges, thisBdry, xfac(n)) 
             reset!(thisBdry)
         end
         
@@ -235,7 +181,7 @@ end
 
 
 function growFromSeed(graph, seed::Int, ncomps::Int, comp, 
-    treeEdges::fastQueue, thisBdry::fastPairQueue, potSeeds::fastQueue, xfac::Float64)
+    treeEdges::fastQueue, thisBdry::fastPairQueue, xfac::Float64)
    
     bdry = 0
     vol = 0
@@ -243,16 +189,10 @@ function growFromSeed(graph, seed::Int, ncomps::Int, comp,
     comp[seed] = ncomps
     for ind in graph.colptr[seed]:(graph.colptr[seed+1]-1)
         nbr = graph.rowval[ind]
-        if comp[nbr] <= 0
-            
-            if comp[nbr] == -1 # if not yet seen at all
-                push!(potSeeds,nbr)
-                comp[nbr] = 0
-            end
+        if comp[nbr] == 0
             
             push!(thisBdry,ind,nbr)
             bdry += 1
-
             vol += 1 # if using sum of degrees CHECK ON THIS
 
         end
@@ -272,17 +212,11 @@ function growFromSeed(graph, seed::Int, ncomps::Int, comp,
                     bdry -= 1
                     vol += 1
 
-                elseif comp[nbr] <= 0
-
-                    if comp[nbr] == -1 # if not yet seen at all
-                        push!(potSeeds,nbr)
-                        comp[nbr] = 0
-                    end
+                elseif comp[nbr] == 0
 
                     push!(thisBdry,ind,nbr)  # issue: nodes pop up many times
                     bdry += 1
                     vol += 1 # if using sum of degrees CHECK ON THIS
-
 
                 end
             end
@@ -320,180 +254,3 @@ function compGraph(graph, comp)
 
 end
 
-#=================================================#
-
-function akpwUsub2(graph, xfac::Function)
-    n = size(graph,1)
-    m = nnz(graph)
-    
-    comp = -ones(Int, n)
-    ncomps = 0
-    
-    # the indices into (ai,aj) = findnz(a) of the edges in the tree
-    treeEdges = fastQueue(n-1)
-    
-    curBdry = fastQueue(n)
-    
-    # start from a random vertex
-    potSeeds = fastQueue(n)
-    push!(potSeeds,rand(1:n))
-    
-    # allocate reusable queue, but only used inside subroutines
-    thisBdry = fastPairQueue(m)
-
-    hackSeed = 1
-    
-    while hackSeed <= n # hasMore(potSeeds)
-
-        # could try pulling a random seed instead
-        # seed = pull!(potSeeds)
-        seed = hackSeed
-        hackSeed += 1
-        
-        if comp[seed] <= 0
-            ncomps += 1
-            growFromSeed2(graph, seed, ncomps, comp, treeEdges, thisBdry, potSeeds, xfac(n)) 
-            reset!(thisBdry)
-        end
-        
-    end
-     
-    
-    #=  THIS CODE CHECKS THAT THE FOREST SPANS THE COMPONENTS 
-    @assert 2*n - nnz(tree) - 2*maximum(comp) == 0
-    for i in 1:maximum(comp)
-        ind = find(comp .== i)
-        tri = tree[ind,ind]
-        @assert isConnected(tri)
-    end
-    =#
-
-    tre = treeEdges.q[1:treeEdges.endPtr]
-
-
-    if maximum(comp) > 1
-
-        cGraph, nextGraph = compGraph2(graph, comp)
-
-        edgeMap = copy(cGraph.nzval)
-#        cGraph.nzval = ones(length(edgeMap))
-
-        if (nnz(cGraph) > 0)
-            ctre = akpwUsub2(nextGraph, xfac)
-
-            sube = edgeMap[ctre]
-
-            tre = [tre;sube]
-        end
-        
-    end
-    
-        
-    return tre
-    
-end
-
-
-
-
-function growFromSeed2(graph, seed::Int, ncomps::Int, comp, 
-    treeEdges::fastQueue, thisBdry::fastPairQueue, potSeeds::fastQueue, xfac::Float64)
-   
-    bdry = 0
-    vol = 0
-    
-    comp[seed] = ncomps
-    for ind in graph.colptr[seed]:(graph.colptr[seed+1]-1)
-        nbr = graph.rowval[ind]
-        if comp[nbr] <= 0
-            
-            if comp[nbr] == -1 # if not yet seen at all
-                push!(potSeeds,nbr)
-                comp[nbr] = 0
-            end
-            
-            push!(thisBdry,ind,nbr)
-            bdry += graph.nzval[ind]
-
-            vol += graph.nzval[ind] # if using sum of degrees CHECK ON THIS
-
-        end
-    end
-
-    while (bdry > xfac*vol) && hasMore(thisBdry)
-        (edge,node) = pull!(thisBdry)
-        
-        if (comp[node] != ncomps)
-
-            comp[node] = ncomps 
-            push!(treeEdges,edge)
-
-            for ind in graph.colptr[node]:(graph.colptr[node+1]-1)
-                nbr = graph.rowval[ind]
-                if comp[nbr] == ncomps
-                    bdry -= graph.nzval[ind]
-                    vol += graph.nzval[ind]
-
-                elseif comp[nbr] <= 0
-
-                    if comp[nbr] == -1 # if not yet seen at all
-                        push!(potSeeds,nbr)
-                        comp[nbr] = 0
-                    end
-
-                    push!(thisBdry,ind,nbr)  # issue: nodes pop up many times
-                    bdry += graph.nzval[ind]
-                    vol += graph.nzval[ind] # if using sum of degrees CHECK ON THIS
-
-
-                end
-            end
-        end
-    end
-
-end
-
-
-      
-function compGraph2(graph, comp)
-
-    m = nnz(graph)
-    (ai,aj,av) = findnz(graph)
-
-    aic = comp[ai]
-    ajc = comp[aj]
-
-    nc = maximum(comp)
-
-    combine(a,b) = a
-
-    aind = collect(1:m)
-
-    # remove self-loops
-    for i in 1:m
-        if aic[i] == ajc[i]
-            aind[i] = 0
-            av[i] = 0
-        end
-    end
-    
-    cGraph = sparse(aic,ajc,aind,nc,nc,combine)
-
-    nextGraph = sparse(aic,ajc,av,nc,nc)
-    
-
-    return cGraph, nextGraph
-
-end
-
-function akpwU2(graph, xfac::Function)
-    n = size(graph,1)
-
-    tre = akpwUsub2(graph, xfac)
-
-    (ai,aj,av) = findnz(graph)
-    tree = sparse([ai[tre];aj[tre]], [aj[tre];ai[tre]], [av[tre];av[tre]])
-
-    return tree
-
-end
