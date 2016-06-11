7d921af7b6038fc5da92aa28abd0e22a1ca835ec
Dan Spielman <spielman@cs.yale.edu>
2016-06-11 00:20:09 -0400

    now all output to file

--- devel/testTrees.jl
+++ devel/testTrees.jl
@@ -103,7 +103,6 @@ for i in 1:nruns
 end
        
 
-    close(h)
 
 mist = minimum([st0 st2 st4 stk stp stold],2)
 mint = minimum([t0 t2 t4  tk tp told],2)
@@ -114,21 +113,22 @@ f = 1.3
 stats(v) = [mean(v./mist) median(v./mist) maximum(v./mist) mean(v .== mist) mean(v .> f*mist)]
 statt(v) = [mean(v./mint) median(v./mint) maximum(v./mint) mean(v .== mint) mean(v .> f*mint)]
 
-println("st0 : ", stats(st0))
-println("st2 : ", stats(st2))
-println("st4 : ", stats(st4))
-#println("st5 : ", stats(st5))
-println("stp : ", stats(stp))
-println("stk : ", stats(stk))
-println("stold : ", stats(stold))
+println(h, "st0 : ", stats(st0))
+println(h, "st2 : ", stats(st2))
+println(h, "st4 : ", stats(st4))
+#println(h, "st5 : ", stats(st5))
+println(h, "stp : ", stats(stp))
+println(h, "stk : ", stats(stk))
+println(h, "stold : ", stats(stold))
 
-println("t0 : ", statt(t0))
-println("t2 : ", statt(t2))
-println("t4 : ", statt(t4))
-#println("t5 : ", statt(t5))
-println("tp : ", statt(tp))
-println("tk : ", statt(tk))
-println("told : ", statt(told))
+println(h, "t0 : ", statt(t0))
+println(h, "t2 : ", statt(t2))
+println(h, "t4 : ", statt(t4))
+#println(h, "t5 : ", statt(t5))
+println(h, "tp : ", statt(tp))
+println(h, "tk : ", statt(tk))
+println(h, "told : ", statt(told))
 
+    close(h)
 
 end
