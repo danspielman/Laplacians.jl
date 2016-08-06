01567a2d841b986791bfc9e5ea1ddbff141bfdee
Serban Stan <serbanandrei.stan@gmail.com>
2016-08-06 16:45:02 -0400

    compareSolvers is now more elegant

--- speedTests/compareSolvers.jl
+++ speedTests/compareSolvers.jl
@@ -1,3 +1,95 @@
+# totTime should be in hours
+function compareSolversOut(n, solvers, tol, totTime)
+    
+    # first, force a compile
+    tab = compareSolversRuns(1000, solvers, tol, 2)
+
+    tab = compareSolversTime(n, solvers, tol, totTime)
+    nruns = size(tab,1) - 1
+
+    fn = string("compSolvers", n , "x", nruns, ".csv")
+    h = open(fn,"w")
+    write(h, chomp(readall(`hostname`)))
+    write(h, "  ")
+    write(h, string(now()))
+    write(h,"\n")
+
+    appendcsv(fn,tab)
+
+end
+
+
+compareSolversRuns(n, solvers, tol, nruns, maxtime=Inf) = compareSolvers(n, solvers, tol=tol, nruns=nruns, maxtime=maxtime)
+compareSolversTime(n, solvers, tol, totTime) = compareSolvers(n, solvers, tol=tol, totTime=totTime)
+
+# totTime comes in hours, convert to seconds
+function compareSolvers(n, solvers; tol=1e-3, nruns=10^8, totTime=Inf, maxtime=totTime*60*60/10)
+
+    totTime = totTime*60*60
+
+    numSolvers = length(solvers)
+    bt = []
+    st = []
+    acc = []
+    name = Array{ASCIIString,1}(0)
+    for i in 1:numSolvers
+        push!(bt, Array{Float64,1}(0))
+        push!(st, Array{Float64,1}(0))
+        push!(acc, Array{Float64,1}(0))
+        push!(name, split(string(solvers[i]),'.')[2])
+    end
+    nedges = Array(Int64,0)
+    nlist = Array(Int64,0)
+    ilist = Array(Int64,0)
+
+
+    t1 = time()
+    for run in 1:nruns
+        if time() > t1 + totTime
+            break
+        end
+
+        a = wtedChimera(n, run)
+        la = lap(a)
+
+        b = randn(n)
+        b = b - mean(b)
+
+        push!(nlist, n)
+        push!(ilist, run)
+        push!(nedges, nnz(a))
+
+        for i in 1:numSolvers
+
+            tic()
+            f = solvers[i](a, tol=tol, maxtime=maxtime)
+            push!(bt[i], toq());
+
+            tic()
+            x = f(b);
+            push!(st[i], toq());
+
+            push!(acc[i], norm(la * x - b) / norm(b))
+
+        end
+
+        print(".")
+
+    end
+
+    tab = ["nlist"; nlist]
+    tab = [tab ["ilist"; ilist]]
+    tab = [tab ["nedges"; nedges]]
+
+    for i in 1:numSolvers
+        tab = [tab [name[i] * "Build"; bt[i]]]
+        tab = [tab [name[i] * "SolveTime"; st[i]]]
+        tab = [tab [name[i] * "RelativeError"; acc[i]]]
+    end
+
+    return tab
+end
+
 function appendcsv(filename, data)
 
     (n1,n2) = size(data)
@@ -15,166 +107,3 @@ function appendcsv(filename, data)
 
     close(fh)
 end
-
-
-
-
-
-# totTime should be in hours
-function compareSolversOut(n, totTime)
-    
-    # first, force a compile
-    tab = compareSolversRuns(1000,2)
-
-    tab = compareSolversTime(n, totTime)
-    nruns = size(tab,1)-1
-    fn = string("compSolvers", n , "x", nruns, ".csv")
-    h = open(fn,"w")
-    write(h, chomp(readall(`hostname`)))
-    write(h, "  ")
-    write(h, string(now()))
-    write(h,"\n")
-
-    appendcsv(fn,tab)
-
-end
-
-
-compareSolversRuns(n, nruns, maxtime=Inf) = compareSolvers(n, nruns=nruns, maxtime=maxtime)
-compareSolversTime(n, totTime) = compareSolvers(n, totTime=totTime)
-
-mst(a) = kruskal(a)
-
-# totTime comes in hours, convert to seconds
-function compareSolvers(n; nruns=10^8, totTime=Inf, maxtime=totTime*60*60/10)
-
-    totTime = totTime*60*60
-
-    akpwBuild = Array(Float64,0)
-    akpwSolve = Array(Float64,0)
-    mstBuild = Array(Float64,0)
-    mstSolve = Array(Float64,0)
-    augBuild = Array(Float64,0)
-    augSolve = Array(Float64,0)
-    cgBuild = Array(Float64,0)
-    cgSolve = Array(Float64,0)
-
-    nedges = Array(Int64,0)
-    nlist = Array(Int64,0)
-    ilist = Array(Int64,0)
-
-    i = 0
-    t1 = time()
-
-    while (i < nruns) && (time() <= t1+totTime)
-        i = i + 1
-        a = wtedChimera(n,i)
-        la = lap(a)
-        
-        b = randn(n)
-        b = b - mean(b)
-
-        push!(nlist, n)
-        push!(ilist ,i)
-        push!(nedges, nnz(a))
-        
-        tic()
-        f = treeSolver(a,akpw)
-        t = toq()
-        push!(akpwBuild, t)
-        
-        tic()
-        x = f(b, maxtime=maxtime)
-        t = toq()
-        push!(akpwSolve, t)
-        
-        tic()
-        f = treeSolver(a,mst)
-        t = toq()
-        push!(mstBuild, t)
-        
-        tic()
-        x = f(b, maxtime=maxtime)
-        t = toq()
-        push!(mstSolve, t)
-        
-        tic()
-        f = augTreeLapSolver(la)
-        t = toq()
-        push!(augBuild, t)
-        
-        tic()
-        x = f(b, maxtime=maxtime)
-        t = toq()
-        push!(augSolve, t)
-        
-        tic()
-        f = cgSolver(la)
-        t = toq()
-        push!(cgBuild, t)
-        
-        tic()
-        x = f(b, maxtime=maxtime)
-        t = toq()
-        push!(cgSolve, t)
-        
-        print(".")
-    end
-
-    tab = ["nlist"; nlist]
-    tab = [tab ["ilist"; ilist]]
-    tab = [tab ["nedges"; nedges]]
-    tab = [tab ["akpwBuild"; akpwBuild]]
-    tab = [tab ["akpwSolve"; akpwSolve]]
-    tab = [tab ["mstBuild"; mstBuild]]
-    tab = [tab ["mstSolve"; mstSolve]]
-    tab = [tab ["augBuild"; augBuild]]
-    tab = [tab ["augSolve"; augSolve]]
-    tab = [tab ["cgBuild"; cgBuild]]
-    tab = [tab ["cgSolve"; cgSolve]]
-
-    #=
-    tab = makeColumn(:nlist)
-    tab = [tab makeColumn(:ilist)]
-    tab = [tab makeColumn(:nedges)]
-    tab = [tab makeColumn(:akpwBuild)]    
-    tab = [tab makeColumn(:akpwSolve)]  
-    tab = [tab makeColumn(:primBuild)]    
-    tab = [tab makeColumn(:primSolve)]  
-    tab = [tab makeColumn(:augBuild)]    
-    tab = [tab makeColumn(:augSolve)]  
-    =#
-
-    return tab
-end
-
-
-function treeSolver(a,treeAlg;maxtime=Inf)
-    t = treeAlg(a)
-    la = lap(a)
-    lt = lap(t)
-    f = pcgLapSolver(la,lt;maxtime=maxtime)
-end
-
-
-# try permuting before factorization
-# this seems to give a slightly faster solve, although it takes longer to build.
-# and, need to figure out how to get the order
-function treeSolverp(a,treeAlg=akpw;maxtime=Inf)
-    n = size(a,1)
-    t = treeAlg(a)
-    lt = lap(t)
-    ord = Laplacians.bfsOrder(t,1);
-    ord = ord[n:-1:1]
-    ltord = lt[ord,ord];
-
-    iord = zeros(Int,size(ord));
-    iord[ord] = collect(1:n);
-    
-    fordp = cholfact(ltord)
-    laord = la[ord,ord]
-    f1 = pcgLapSolver(laord,ltord)    
-
-    fp(b; maxtime=maxtime) = f1(b[ord], maxtime=maxtime)[iord]
-end
-
