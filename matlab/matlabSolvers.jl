54d4e7a5978ce0d7ddf4c4145813edf915c097ec
Dan Spielman <spielman@cs.yale.edu>
2016-11-25 19:07:08 -0500

    added (and separated) Lap and SDD solvers.

--- matlab/matlabSolvers.jl
+++ matlab/matlabSolvers.jl
@@ -30,7 +30,7 @@ function matlabIccLapSolver(a, b; tol::Real=1e-6, maxits=10000)
     @mput b
 
     mat"""
-    tic; f = iccSolver(la,[],opts); icc_build_time = toc
+    tic; f = lapIccSolver(la,[],opts); icc_build_time = toc
 
     tic; x = f(b); icc_solve_time = toc;
     err = norm(la*x-b)/norm(b);
@@ -61,10 +61,69 @@ function matlabCMGLapSolver(a, b; tol::Real=1e-6, maxits=10000)
     @mput b
 
     mat"""
-    tic; f = cmgSolver(la,[],opts); cmg_build_time = toc
+    tic; f = lapCmgSolver(la,[],opts); cmg_build_time = toc
 
     tic; x = f(b); cmg_solve_time = toc;
     err = norm(la*x-b)/norm(b);
+    """
+
+    @mget x
+    @mget err
+    @mget cmg_build_time
+    @mget cmg_solve_time
+    
+    return x, err, cmg_build_time, cmg_solve_time
+end
+
+"""
+    x, err, icc_build, icc_solve = matlabIccSDDSolver(a, b; tol, maxits)
+
+Calls an incomplete Cholesky factorization solver from Matlab.
+Before using this, you need to include setUpMatlab.jl
+"""
+function matlabIccSDDSolver(sdd, b; tol::Real=1e-6, maxits=10000)
+    
+    opts = Opts(tol,maxits)
+
+    @mput opts
+    @mput sdd
+    @mput b
+
+    mat"""
+    tic; f = iccSolver(sdd,[],opts); icc_build_time = toc
+
+    tic; x = f(b); icc_solve_time = toc;
+    err = norm(sdd*x-b)/norm(b);
+    """
+
+    @mget x
+    @mget err
+    @mget icc_build_time
+    @mget icc_solve_time
+    
+    return x, err, icc_build_time, icc_solve_time
+end
+
+"""
+    x, err, cmg_build, cmg_solve = matlabCMGDDSolver(a, b; tol, maxits)
+
+Calls an incomplete Cholesky factorization solver from Matlab.
+Before using this, you need to include setUpMatlab.jl.
+This requires Yiannis Koutis's CMG solver.
+"""
+function matlabCMGSDDSolver(sdd, b; tol::Real=1e-6, maxits=10000)
+    
+    opts = Opts(tol,maxits)
+
+    @mput opts
+    @mput sdd
+    @mput b
+
+    mat"""
+    tic; f = cmgSolver(sdd,[],opts); cmg_build_time = toc
+
+    tic; x = f(b); cmg_solve_time = toc;
+    err = norm(sdd*x-b)/norm(b);
     """
 
     @mget x
