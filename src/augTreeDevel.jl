21c6c623159f1d257a3b3c93020948dc073172ae
Dan Spielman <spielman@cs.yale.edu>
2016-12-29 12:33:26 -0500

    put in speed testing routines, so now can compare

--- src/augTreeDevel.jl
+++ src/augTreeDevel.jl
@@ -229,4 +229,99 @@ function augTreeLapSolver3{Tv,Ti}(a::SparseMatrixCSC{Tv,Ti}; tol::Real=1e-6, max
 
 end
 
+
+#==========================================================
+Speed Testing
+===========================================================#
     
+
+using DataFrames
+
+global speedDF = DataFrame()
+
+
+type SolverTest
+    solver::Function
+    name::String
+end
+
+
+
+function initDictCol!(dic, name, typ)
+    if ~haskey(dic,name)
+        dic[name] = Array(typ,0)
+    end
+end
+
+
+
+"""
+
+    function speedTestLapSolvers{Tv,Ti}(solvers, dic, a::SparseMatrixCSC{Tv,Ti}, b::Array{Tv,1}; tol::Real=1e-6, maxits=Inf, maxtime=Inf, verbose=false)
+
+Runs many Laplacians solvers.  Puts the build and solve time results into solverDF.  Returns the answer given by KMP.
+
+"""
+function speedTestLapSolvers{Tv,Ti}(solvers, dic, a::SparseMatrixCSC{Tv,Ti}, b::Array{Tv,1}; tol::Real=1e-2, maxits=Inf, maxtime=Inf, verbose=false)
+
+
+    b = b - mean(b)
+    
+    la = lap(a)
+
+    it = Int[1]
+
+    # init the dict, if it is the first time
+    initDictCol!(dic, "nv", Int)
+    initDictCol!(dic, "ne", Int)
+    initDictCol!(dic, "hash_a", UInt64)
+    
+    solvecol(name) = "$(name)_solve"
+    buildcol(name) = "$(name)_build"
+    totcol(name) = "$(name)_tot"
+    itscol(name) = "$(name)_its"
+    errcol(name) = "$(name)_err"
+    
+    for solver in solvers
+        name = solver.name
+        initDictCol!(dic, solvecol(name), Float64)
+        initDictCol!(dic, buildcol(name), Float64)
+        initDictCol!(dic, totcol(name), Float64)
+        initDictCol!(dic, itscol(name), Float64)
+        initDictCol!(dic, errcol(name), Float64)
+    end
+    
+    nv = size(a,1)
+    ne = nnz(a)
+    hash_a = hash(a)
+
+    push!(dic["nv"],nv)
+    push!(dic["ne"],ne)
+    push!(dic["hash_a"],hash_a)
+    
+    for solverTest in solvers
+        tic()
+        f = solverTest.solver(a, tol=tol, maxtime=maxtime, maxits=maxits, verbose=verbose)
+        build_time = toq()
+
+        tic()
+        x = f(b, pcgIts = it)
+        solve_time = toq()
+
+        err = norm(la * x - b) / norm(b)
+
+        name = solverTest.name
+        push!(dic[solvecol(name)],solve_time)
+        push!(dic[buildcol(name)],build_time)
+        push!(dic[totcol(name)],solve_time + build_time)
+        push!(dic[itscol(name)],it[1])
+        push!(dic[errcol(name)],err)
+
+    end
+
+    return x
+
+end
+
+
+
