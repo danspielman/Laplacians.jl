3504688d93e7aaa31f6e7b0cc0643bf2e02a9d3d
Dan Spielman <spielman@cs.yale.edu>
2017-07-18 14:53:14 -0400

    Laplacians 0.2.0 should be compatible with Julia 0.6

--- docs/make.jl
+++ docs/make.jl
@@ -8,5 +8,5 @@ makedocs(modules=[Laplacians], doctest = false)
 deploydocs(
            repo = "github.com/danspielman/Laplacians.jl.git",
            deps   = Deps.pip("mkdocs", "python-markdown-math"),
-           julia  = "0.5.0"
+           julia  = "0.6"
 )
