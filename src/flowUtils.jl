4636bd9366312d84d807142e9ac593c8b42d4bc1
Dan Spielman <spielman@cs.yale.edu>
2017-09-18 11:20:17 -0400

    adding flow problem generators

--- src/flowUtils.jl
+++ src/flowUtils.jl
@@ -19,6 +19,33 @@ type MCFproblem{Tv,Ti}
     demands::Array{Tv,1}
 end
 
+
+"""
+  A Max Flow Problem is specified by an edge list (from to) for each,
+  capacities on edges,
+  and a demands on vertices that sum to 0
+"""
+type MaxFlowProblem{Tv,Ti}
+    edge_list::Array{Ti,2}
+    capacities::Array{Tv,1}
+    demands::Array{Tv,1}
+end
+
+"""
+  An st Flow Problem is specified by an edge list (from to) for each,
+  capacities on edges,
+  s and t
+"""
+type stFlowProblem{Tv,Ti}
+    edge_list::Array{Ti,2}
+    capacities::Array{Tv,1}
+    s::Int
+    t::Int
+end
+
+
+
+
 function reportMCFresults(mcfp, flow)
     println("Cost: ", (mcfp.costs'*flow)[1])
     println("Min flow: ", minimum(flow))
@@ -32,6 +59,54 @@ function reportMCFresults(mcfp, flow)
 
     println("Error on demands: ", sum(abs.(B'*flow- mcfp.demands)))
 end
+
+
+function check_flow(stfp::stFlowProblem, f; tol=1e-3)
+    n = maximum(stfp.edge_list)
+    m = size(stfp.edge_list,1)
+    
+    obj = zeros(m)
+    idx = find(stfp.edge_list[:,1].==stfp.s)
+    obj[idx] = 1
+
+    value = obj'*f
+    println("Value: ", value)
+
+    unsaturated = f .< (1-tol)*stfp.capacities
+
+    usei = stfp.edge_list[unsaturated,1]
+    usej = stfp.edge_list[unsaturated,2]
+    adir = sparse(usej,usei,1,n,n) # note backwards convention
+    comp = reachFrom(adir,stfp.s)
+
+    a = sparse(stfp.edge_list[:,1],stfp.edge_list[:,2],stfp.capacities,n,n)
+    cut_val = cutCapacity(a,comp)
+
+    println("The cut value is ", cut_val)
+
+    scut = sum(stfp.capacities[stfp.edge_list[:,1] .== stfp.s])
+    tcut = sum(stfp.capacities[stfp.edge_list[:,2] .== stfp.t])
+    println("cut {s} has value $(scut) and cut {t} has value $(tcut)")
+
+    if any(f.<0) 
+        println("most negative flow: ", minimum(f[f.<0]))
+    end
+    over = f-stfp.capacities
+    if any(over.>0)
+        println("most over capacity: ", maximum(over[over.>0]))
+    end
+
+    B = sparse(collect(1:m), stfp.edge_list[:,1], 1.0, m, n) -
+      sparse(collect(1:m), stfp.edge_list[:,2], 1.0, m, n)
+    B[:,stfp.s] = 0
+    B[:,stfp.t] = 0
+    outflow = f'*B
+    println("flow-in = flow-out failure: ",maximum(abs.(outflow)))      
+            
+    return value
+
+end
+
 
 
 """
@@ -95,32 +170,99 @@ function readDimacsMCF(filename)
     return mcfp
 end
 
+"""
+    v = quant_vec(x, maxint)
 
+Quantize the vector x to integers of absolute value at most maxint.
+"""
+function quant_vec(v, maxint)
+    maxv = maximum(abs.(v))
+    if maxv == 0
+        return round.(Int,v)
+    elseif (maxv < maxint) && (maximum(v-round.(Int,v)) == 0)
+        return round.(Int,v)
+    else
+        f(x) = round(Int,sign(x))*ceil(Int, maxint * abs(x) / maxv)
+        return f.(v)
+    end
+end
 
 """
-  A Max Flow Problem is specified by an edge list (from to) for each,
-  capacities on edges,
-  and a demands on vertices that sum to 0
+   writeDimacsMCF(filename, mcfp)
+
+Write a minimum cost flow problem in the <a href="http://lpsolve.sourceforge.net/5.5/DIMACS_mcf.htm">Dimacs format</a>.
 """
-type MaxFlowProblem{Tv,Ti}
-    edge_list::Array{Ti,2}
-    capacities::Array{Tv,1}
-    demands::Array{Tv,1}
+function writeDimacsMCF(filename, mcfp)
+
+    fi = open(filename, "w")
+
+    edge_list = mcfp.edge_list
+    m = size(edge_list,1)
+    n = maximum(edge_list)
+
+    # the problem line
+    write(fi, "p min $(n) $(m)\n")
+
+
+    dems = round.(Int32, quant_vec(mcfp.demands, round(Int(2^31-1))))
+    for i in 1:n
+        write(fi, "n $(i) $(dems[i])\n")
+    end
+
+    caps = round.(Int32, quant_vec(mcfp.capacities, 2^31-1))
+    costs = round.(Int32, quant_vec(mcfp.costs, 2^31-1))
+    
+    for i in 1:m
+        write(fi, "a $(edge_list[i,1]) $(edge_list[i,2]) 0 $(caps[i]) $(costs[i])\n")
+    end
+
+    close(fi)
 end
 
 
 
+"""
+    prob = makeSTFlowProblem(a; k=round(Int,sqrt(size(a,1))), choice=:farpair)
 
-function makeSTFlowProblem(a; k=round(Int,sqrt(size(a,1))))
+Create an instance of an stFlowProblem with one source and once sink
+from a graph given by its Adjacency matrix `a` where the weights are capacities.
+It creates two new vertices s and t.
+These are each connected to `k` vertices in the graph.
+These k vertices are chosen according to `choice`.
+
+* :farpair means choose one vertex and the k closest to it.  Then choose the vertex farthese from that, and the k closest to it.
+* :spect means approximate a fiedler vector, use it to order, and choose the extreme k.
+* :rand choose those k at random
+
+"""
+function makeSTFlowProblem(a; k=round(Int,sqrt(size(a,1))), choice=:farpair)
 
     n = size(a,1)
-    s0 = rand(1:n)
-    ds0 = shortestPaths(a,s0)[1]
-    sk = sortperm(ds0)[1:k]
 
-    t0 = findmax(ds0)[2]
-    dt0 = shortestPaths(a,t0)[1]
-    tk = sortperm(dt0)[1:k]
+    if choice == :farpair
+
+        s0 = rand(1:n)
+        ds0 = shortestPaths(a,s0)[1]
+        sk = sortperm(ds0)[1:k]
+
+        t0 = findmax(ds0)[2]
+        dt0 = shortestPaths(a,t0)[1]
+        tk = sortperm(dt0)[1:k]
+
+
+    elseif choice == :spect
+        f = approxCholLap(a)
+        op = SqLinOp(true,1.0,size(a,1),f)
+        v = vec(eigs(op;nev=1,which=:LM)[2])
+        p = sortperm(v)
+        sk = p[1:k]
+        tk = p[(n-k+1):n]
+
+    else  # choice == :rand
+        p = randperm(n)
+        sk = p[1:k]
+        tk = p[(k+1):(2*k)]
+    end
     
     deg = sum(a,2)
     
@@ -129,26 +271,67 @@ function makeSTFlowProblem(a; k=round(Int,sqrt(size(a,1))))
     anew = spzeros(n+2,n+2)
     
     anew[1:n,1:n] = a
-    anew[sk,s] = deg[sk]
+    # anew[sk,s] = deg[sk]
     anew[s,sk] = deg[sk]
     anew[tk,t] = deg[tk]
-    anew[t,tk] = deg[tk]
+    # anew[t,tk] = deg[tk]
 
-#=    
-    demand = min(sum(deg[tk]), sum(deg[sk]))
-    
-    BBt = -dirEdgeVertexMat(anew)'
-
-    u = copy(anew.nzval)
-    b = zeros(n+2)
-    b[s]=demand
-    b[t]=-demand
-=#
+    (ai,aj,av) = findnz(anew)
+    return stFlowProblem([ai aj],av,s,t)
 
     return anew, s, t
-#    return BBt, u, b
 end
 
+
+"""
+    mcfp = stf_to_mcf(stfp::stFlowProblem)
+
+Recast an st flow problem as a min cost flow problem.
+It adds one edge at the end, so to recover the st flow one needs to remove it
+"""
+function stf_to_mcf(stf::stFlowProblem)
+
+    n = maximum(stfp.edge_list)
+    m = size(stfp.edge_list,1)
+
+    edges = zeros(Int, m+1, 2)
+    for i in 1:m
+        edges[i,:] = stfp.edge_list[i,:]
+    end
+    edges[m+1,:] = [stfp.t stfp.s]
+
+    costs = zeros(m+1)
+    costs[m+1] = -1
+
+    dems = zeros(n)
+    caps = copy(stfp.capacities)
+
+    push!(caps,sum(caps[find(edges[:,1].==stfp.s)]))
+
+    mcfp = MCFproblem(edges, caps, costs, dems)
+end
+
+
+function Float(mcfp::MCFproblem)
+    return MCFproblem(mcfp.edge_list, Float64.(mcfp.capacities), Float64.(mcfp.costs), Float64.(mcfp.demands))
+end
+
+
+function rand_mcfp(a; maxdem=1000, maxcost=1000, maxcap=1000)
+
+    n = size(a,1)
+    
+    ai, aj, av = findnz(a)
+    m = length(ai)
+    caps = quant_vec(av, maxcap)
+    
+    dems = zeros(Int,n)
+
+    costs = quant_vec( (rand(m)-0.5), maxcost)
+
+    mcfp = MCFproblem([ai aj], caps, costs, dems)
+    
+end
 
 
 function stFlow(dirg, s0, t0; lapSolver = cholLap, tol=1e-6)
@@ -206,7 +389,7 @@ function reachFrom{Tv,Ti}(dirg::SparseMatrixCSC{Tv,Ti}, v0::Ti)
     n = dirg.n
 
     comp = zeros(Ti,n)
-    order = Array(Ti,n)
+    order = Array{Ti}(n)
 
     # note that all of this casting is unnecessary.
     # but, some of it speeds up the code
