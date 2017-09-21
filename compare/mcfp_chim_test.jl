e4ffefdbe2a2fbca4f99fccacf8752e70526423e
Dan Spielman <spielman@cs.yale.edu>
2017-09-20 20:38:01 -0400

    code for comparing mcf algorithms

--- compare/mcfp_chim_test.jl
+++ compare/mcfp_chim_test.jl
@@ -3,7 +3,7 @@ hours = parse(Float64,ARGS[2])
 
 t_start = time()
 
-jld = "goto8_$(n)h$(hours).jld"
+jld = "mcfp_chim_$(n)h$(hours).jld"
 println(jld)
 
 using Laplacians
@@ -15,11 +15,13 @@ include("$(lapdir)/src/flow_extern.jl")
 
 
 # warm up run
+k = round(Int,n/20)
+mcfp = gen_mcfp(wtedChimera(n,1), frac_of_max=0.9, k=k, choice = :spect);
+fn = "tmp.txt"
+writeDimacsMCF(fn,mcfp)
 
-fn = goto8_gen(n,1)
 cs2_time, _ = call_cs2(fn)
 lemon_time, _ = call_lemon(fn)
-mcfp = readDimacsMCF(fn);
 t1 = time()
 sol = min_cost_flow(Float(mcfp), lapSolver = h->approxCholSddm(h,tol=1e-8), tol = 1e-5)
 ipm_time = time()-t1
@@ -41,20 +43,21 @@ while time() - t_start < 60*60*hours
     println("----------------------")
     println(i)
 
+    mcfp = gen_mcfp(wtedChimera(n,i), frac_of_max=0.9, k=k, choice = :spect);
+    fn = "tmp.txt"
+    writeDimacsMCF(fn,mcfp)
 
-    fn = goto8_gen(n,i)
     @show now()
     cs2_time, _ = call_cs2(fn)
     @show now()
     lemon_time, _ = call_lemon(fn)
-    mcfp = readDimacsMCF(fn);
     @show now()
     t1 = time()
-    sol = min_cost_flow(Float(mcfp), lapSolver = h->approxCholSddm(h,tol=1e-8), tol = 1e-5, verbose=false)
+    sol = min_cost_flow(Float(mcfp), lapSolver = h->approxCholSddm(h,tol=1e-8), tol = 1e-5)
+    ipm_time = time()-t1
 
     rm(fn)
-    
-    ipm_time = time()-t1
+
     push!(dic["lemon"], lemon_time)
     push!(dic["cs2"], cs2_time)
     push!(dic["ipm"], ipm_time)
