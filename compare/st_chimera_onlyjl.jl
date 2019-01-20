1d3bba3f12112ccdae4187afecef92f059be731b
Rasmus Kyng <rjkyng@gmail.com>
2019-01-19 22:04:23 -0500

    adding warmup of test code

--- compare/st_chimera_onlyjl.jl
+++ compare/st_chimera_onlyjl.jl
@@ -6,6 +6,15 @@ Which will run for approximately 10 hours on graphs with 123456 vertices
 
 =#
 
+# tprepre = time()
+
+# __precompile()__
+
+# @printf("precompile time = %.1f\n",tpre-tprepre)
+
+
+tpre = time()
+
 n = Base.parse(Int64,ARGS[1])
 hours = Base.parse(Float64,ARGS[2])
 
@@ -19,10 +28,14 @@ using Statistics
 using LinearAlgebra
 using Printf
 
+#diagnostics
+using InteractiveUtils
+using Profile
+
 lapdir = dirname(pathof(Laplacians))
 
-include("$(lapdir)/../compare/matlabSafe.jl")
-include("$(lapdir)/../compare/trilinosDrivers.jl")
+#include("$(lapdir)/../compare/matlabSafe.jl")
+#include("$(lapdir)/../compare/trilinosDrivers.jl")
 include("$(lapdir)/../compare/compare_solvers_TL.jl")
 
 
@@ -45,22 +58,41 @@ test_acw = SolverTest(ac_wdeg, "ac_fast")
 #tests = [test_ac test_acw]
 
     tests = [test_acw]
+
+run_icc = false
+run_cmg = false
+run_lamg = false
+run_muelubelos = false
     
-# warm up the code
-a = chimera(10000,1)
-b = randn(10000)
+# warm up the solver code
+a = chimera(1000,1)
+b = randn(1000)
 b = b .- mean(b)
 for solver in tests
     f = solver.solver(a,verbose=true)
     x = f(b)
 end
 
+# warm up the test code
+using JLD2
+tn = "chimera(1000,1)"
+dicWarmup = Dict() 
+@show @elapsed x = testVMatlabLap(tests, dicWarmup, a, b, verbose=true, tol=1e-8, testName=tn, test_icc=run_icc, test_cmg=run_cmg, test_lamg=run_lamg, test_muelubelos=run_muelubelos )
+@show @elapsed @save "warmupdummy.jld" dicWarmup
+# warm up the test code again?
+dicWarmup = Dict() 
+@show @elapsed x = testVMatlabLap(tests, dicWarmup, a, b, verbose=true, tol=1e-8, testName=tn, test_icc=run_icc, test_cmg=run_cmg, test_lamg=run_lamg, test_muelubelos=run_muelubelos )
+@show @elapsed @save "warmupdummy.jld" dicWarmup
+
+    
 dic = Dict()
 
-using JLD2
+
+
 
 i = 0
 t0 = time()
+@printf("test load&warmup time = %.1f\n",t0-tpre)
 
 while time() - t0 < 60*60*hours
 
@@ -71,20 +103,24 @@ while time() - t0 < 60*60*hours
 
     @time a = chimera(n,i)
     @show Base.summarysize(a)
-    @time aw = randWeight(a)
+    @time aw = rand_weight(a)
     @show Base.summarysize(aw)
 
     tn = "chimera($n,$i)"
-    b = randn(n)
-    b = b .- mean(b)
-    b = b / norm(b)
-    x = testVMatlabLap(tests, dic, a, b, verbose=true, tol=1e-8, testName=tn, test_icc=false, test_cmg=false, test_lamg=false, test_muelubelos=false )
+    @time b = randn(n)
+    @time b = b .- mean(b)
+    @time b = b / norm(b)
+    x = testVMatlabLap(tests, dicWarmup, a, b, verbose=true, tol=1e-8, testName=tn, test_icc=run_icc, test_cmg=run_cmg, test_lamg=run_lamg, test_muelubelos=run_muelubelos )
     @save fn dic
-    tn = "wtedChimera($n,$i)"
-    x = testVMatlabLap(tests, dic, aw, b, verbose=true, tol=1e-8, testName=tn, test_icc=false, test_cmg=false, test_lamg=false, test_muelubelos=false )
-    @save fn dic
+    #tn = "wtedChimera($n,$i)"
+    #x = testVMatlabLap(tests, dic, aw, b, verbose=true, tol=1e-8, testName=tn, test_icc=false, test_cmg=false, test_lamg=false, test_muelubelos=false )
+    #@save fn dic
 
     @printf("time (sec) this iter = %.1f\n",time() - ti)
     @printf("time (sec) all  iter = %.1f\n",time() - t0)
 
 end
+
+# li, lidict = Profile.retrieve()
+# using JLD
+# @save "/tmp/foo.jlprof" li lidict
